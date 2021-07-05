f57a1d1d3e340c4405a8af1d72caa236f049e28a
mark craven <mpcrav01@gmail.com>
2021-07-05 12:21:09 -0400

    move css to scss file to prep for mediaqueries

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
   <ul class="navInternal">
     <!-- TODO: change to font-awesome svgs -->
-    <li @click="scrollToSection('home')">Home</li>
-    <li @click="scrollToSection('about')">About</li>
-    <li @click="scrollToSection('portfolio')">Portfolio</li>
-    <li @click="scrollToSection('cin')">Contact</li>
+    <div class="navContent">
+      <li @click="scrollToSection('home')">Home</li>
+      <li @click="scrollToSection('about')">About</li>
+      <li @click="scrollToSection('portfolio')">Portfolio</li>
+      <li @click="scrollToSection('cin')">Contact</li>
+    </div>
   </ul>
 </template>
 
 <script>
+//TODO: add wrapper over list items that only covers ~50% of nav. can justify-content in there to give items desired spacing
 export default {
   name: "Navbar",
   methods: {
@@ -22,28 +25,39 @@ export default {
 </script>
 
 <style>
+@import "../css/styles.css";
+
 .navInternal {
   padding: 0;
+  margin: 0;
+  list-style: none;
+  width: 10.5%;
+  background: var(--dark-bg-opaque);
+  height: 100vh;
+  position: fixed;
+  z-index: 100;
+  color: white;
+  transition: width 225ms ease;
+}
+
+.navContent {
+  height: 50%;
+  width: 50%;
   list-style: none;
   display: flex;
   flex-direction: column;
-  margin-top: 200px;
-  margin-bottom: 20px;
-  margin-right: 20px;
-  margin-left: 5px;
   justify-content: space-between;
   align-content: space-between;
-  align-items: center;
-  justify-items: flex-start;
-  height: 50%;
-  color: white;
+  padding-top: 50px;
 }
-
-.navInternal > li {
+.navInternal:hover {
+  width: 14%;
+}
+.navInternal > .navContent > li {
+  list-style: none;
   border-bottom: 1px solid white;
-  width: 100%;
   display: flex;
-  align-items: center;
+  align-self: flex-start;
   cursor: pointer;
 }
 </style>
